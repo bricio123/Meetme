10b90dd6837dc0611483092127da90dc3db86fe6
Fabricio sousa <fabricio1234c@gmail.com>
2020-06-14 15:13:30 -0300

    more modify

--- src/slide.js
+++ src/slide.js
@@ -1,2 +1,26 @@
-
-$('.your-slider').slick('unslick');
+$('icons_slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+              
