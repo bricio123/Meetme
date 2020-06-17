c5eb595fee5116964b235225d44e8387d07750bc
Fabricio sousa <fabricio1234c@gmail.com>
2020-06-17 10:51:46 -0300

    adding slide

--- src/slide.js
+++ src/slide.js
@@ -1,26 +1,7 @@
-$('icons_slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
+$('.jobs').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
   });
-              
